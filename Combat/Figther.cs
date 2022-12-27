71a02674644918f459a918a2fdeac097a91e50c1
Victor Diniz <victor.dnz@hotmail.com>
2022-12-26 21:44:54 -0300

    first commit

---
+++ Combat/Figther.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace RPG.Combat{
+
+    public class Figther : MonoBehaviour
+    {  
+       public void Attack(CombatTarget target){
+        print("Take that you short, squat peasant!");
+       }
+    }
+}
